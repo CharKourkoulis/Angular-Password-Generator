f3e7ab5ea1057dfbfb408b3388a66289d31a314a
unknown <b.kourkoulis@gmail.com>
2020-02-29 20:58:42 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,60 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'pw';
+   password = '';
+   includeLetters = false;
+   includeNumbers = false;
+   includeSymbols = false;
+   length = 0;
+
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()_+-=<>?';
+
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+  }
+
+  onChangedUseLetters() {
+      this.includeLetters = !this.includeLetters;
+  }
+
+  onChangedUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangedUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onChangeLength(value: string) {
+    // tslint:disable-next-line:radix
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+
 }
